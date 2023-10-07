bf320613dc5a81a417c05f46043070236deed05c
BrunoCura2020 <brunocura16@gmail.com>
2023-10-07 12:37:49 -0300

    se crea el proyecto y se agrega go_router

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+import 'package:river_app/presentation/screens/screens.dart';
+
+final appRouter = GoRouter(
+  /* initialLocation: '/', */
+  routes: [
+    GoRoute(
+      /*
+        state: podemos obtener información de la ruta, argumentos
+        context: es el arbol de widget, nos dice todo lo de nuestra app
+       */
+      path: '/', 
+      builder: (context, state) => const HomeScreen()
+    )
+  ] 
+);
